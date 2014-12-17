22a4e3b183f2056d51e21b1624e6bcad791c8397
Fansion <Fansionspace@gmail.com>
2014-12-17 23:53:57 +0800

    增加简单的数据统计显示

--- sharefun/controllers/site.py
+++ sharefun/controllers/site.py
@@ -6,7 +6,7 @@ from flask import render_template, Blueprint, redirect, url_for, request, flash,
 from flask.ext.login import current_user
 
 from ..forms import WorkForm, CommentForm
-from ..models import db, Category, Work, Comment, Recommendation, Status
+from ..models import db, Category, Work, Comment, Recommendation, Status, User
 
 import os
 from datetime import datetime
@@ -19,7 +19,10 @@ def index():
     """首页"""
     works = Work.query.order_by(Work.cate_id, Work.created.desc())
     newest_comments = Comment.query.order_by(Comment.created.desc()).limit(5)
-    return render_template('site/index.html', works=works, newest_comments=newest_comments, current_time=datetime.utcnow())
+    total = Recommendation.query.count()
+    success = Recommendation.query.filter(Recommendation.status_id==3).count()
+    failure = Recommendation.query.filter(Recommendation.status_id==4).count()
+    return render_template('site/index.html', works=works, newest_comments=newest_comments, current_time=datetime.utcnow(), total=total, success=success, failure=failure)
 
 
 @bp.route('/about')
@@ -42,6 +45,12 @@ def work(work_id):
         return redirect(url_for('site.work', work_id=work.id))
     return render_template('site/work.html', work=work, form=form)
 
+@bp.route('/user/<int:id>')
+def user(id):
+    user = User.query.get_or_404(id)
+    success_recomms_count = Recommendation.query.filter(Recommendation.user_id==id, Recommendation.status_id==3).count()
+    comments_count = Comment.query.filter(Comment.user_id==id).count()
+    return render_template('site/user.html', user=user, success_recomms_count=success_recomms_count, comments_count=comments_count)
 
 @bp.route('/comments', defaults={'page': 1})
 @bp.route('/comments/page/<int:page>')
